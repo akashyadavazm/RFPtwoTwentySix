8a9147f49579ee4fb383cc5d852fd0d658da34f0
Akash Yadav <109862673+akashyadavazm@users.noreply.github.com>
2022-12-02 15:45:51 +0530

    Update WindChill.java

--- WindChill.java
+++ WindChill.java
@@ -3,12 +3,17 @@ package day5Problems.functionalPrograms;
 public class WindChill {
 	
 	 public static void main(String[] args) {
-	        double t = Double.parseDouble(args[0]);
-	        double v = Double.parseDouble(args[1]);
-	        double w = 35.74 + 0.6215*t + (0.4275*t - 35.75) * Math.pow(v, 0.16);
-	        System.out.println("Temperature = " + t);
-	        System.out.println("Wind speed  = " + v);
-	        System.out.println("Wind chill  = " + w);
+	       Scanner sc = new Scanner(System.in);
+		System.out.println("Give the value of Temprature ranges \n(T<=50) ");
+		String t1 = sc.next();
+		System.out.println("Give the value of WindSpeed ranges \n(3<V<120)");
+		String v1 = sc.next();
+		double t = Double.parseDouble(t1);
+		double v = Double.parseDouble(v1);
+		double w = 35.74 + 0.6215 * t + (0.4275 * t - 35.75) * Math.pow(v, 0.16);
+		System.out.println("Temperature = " + t);
+		System.out.println("Wind speed  = " + v);
+		System.out.println("Wind chill  = " + w);
 	    }
 
 }
